ac9706c6e29e2732879165619abf3b0546ab2dd1
Emmanuel Makori Obiero <emmanuelmakobiero@gmail.com>
2026-07-07 21:26:22 +0300

    added nitro plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -10,8 +11,8 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart({
       server: { entry: "server" },
-      target: "vercel",
     }),
+    nitro({ preset: "vercel" }),
     react(),
   ],
 });
